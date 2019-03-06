3f12f6d413d40a6fc5af9a3301daa34c5e4d463c
Cédric Lemaitre <c.lemaitre58@gmail.com>
2019-03-06 10:04:53 +0100

    code et data process of 2018 track ok

--- main.py
+++ main.py
@@ -9,55 +9,54 @@ if __name__ == '__main__':
 
     path_data = '/home/cedric/Documents/Ergocycle/data/DataEstACd/20180515/'
 
-    user_data = 'Cedric.csv'
+    l_user_data = ['Guillaume', 'Denis', 'Aurel']
+    l_etalonnage = [1.02, 1, 1.61]
+    l_masse = [88, 78, 78]
+    extension = ".csv"
 
-    l_power_mean, l_speed_mean = extract_with_pandas(path_data + user_data)
+    for j in range(len(l_user_data)):
 
-    # l_power_mean = [117, 139, 165, 212, 240, 283,
-    #                 124, 146, 156, 214, 226, 278]
-    # l_speed_mean = [25.1, 25.5, 30.5, 30.6, 34.8,
-    #                 34.3, 27.1, 26.9, 31.3, 30.3,
-    #                 34.8, 34.7]
+        f_path = path_data + l_user_data[j] + extension
+        print(f_path)
+        l_power_mean, l_speed_mean = extract_with_pandas(f_path)
 
-    l_speed_mean = np.array(l_speed_mean) / 3.6
-    l_power_mean = np.array(l_power_mean) * 0.85
-    l_ACd = []
-    l_f_aero = []
-    # l_wind = []
+        l_speed_mean = np.array(l_speed_mean) / 3.6
+        l_power_mean = np.array(l_power_mean) * 0.85 * l_etalonnage[j]
+        l_ACd = []
+        l_f_aero = []
 
-    # for i in range(len(l_power_mean)):
-    #     if i % 2 == 0:
-    #         val_wind = est_wind(l_power_mean[i], l_power_mean[i+1],
-    #                             l_speed_mean[i], l_speed_mean[i+1])
-    #         l_wind.append(val_wind)
+        for i in range(len(l_power_mean)):
+            val_acd = inv_model_grappe_ACd(l_power_mean[i], l_speed_mean[i],
+                                           0, 20, 1015, 0.004, l_masse[j])
+            val_f_aero = inv_model_grappe_F(l_power_mean[i], l_speed_mean[i],
+                                            0.004, l_masse[j])
+            l_f_aero.append(val_f_aero)
+            l_ACd.append(val_acd)
 
-    # np.array(l_wind)
-    # wind_mean = np.mean(l_wind)
+            l_ACd_f = []
 
-    for i in range(len(l_power_mean)):
-        val_acd = inv_model_grappe_ACd(l_power_mean[i], l_speed_mean[i],
-                                       0, 20, 1015, 0.004, 100)
-        val_f_aero = inv_model_grappe_F(l_power_mean[i], l_speed_mean[i],
-                                        0.004, 100)
-        l_f_aero.append(val_f_aero)
-        l_ACd.append(val_acd)
+        # for i in range(6):
+        #     print("ACd : ", l_ACd[i], "Aero Drag : ", l_f_aero[i])
 
-        l_ACd_f = []
+        # for i in range(len(l_power_mean)):
+        #     if i % 2 == 0:
+        #         moy_acd = (l_ACd[i] + l_ACd[i+1]) / 2
+        #         moy_f = (l_f_aero[i] + l_f_aero[i+1]) / 2
+        #         l_ACd_f.append([moy_acd, moy_f])
 
-    # for i in range(6):
-    #     print("ACd : ", l_ACd[i], "Aero Drag : ", l_f_aero[i])
+        print("User : ", l_user_data[j])
+        print("Valeur main en haut : Vitesse - ACd - F")
+        for i in range(len(l_power_mean) // 3):
+            print(l_speed_mean[i]*3.6, "-", l_ACd[i], " - ", l_f_aero[i])
 
-    # for i in range(len(l_power_mean)):
-    #     if i % 2 == 0:
-    #         moy_acd = (l_ACd[i] + l_ACd[i+1]) / 2
-    #         moy_f = (l_f_aero[i] + l_f_aero[i+1]) / 2
-    #         l_ACd_f.append([moy_acd, moy_f])
+        print("Valeur main aux poignées : Vitesse - ACd - F")
+        for i in range(len(l_power_mean) // 3, 2 * len(l_power_mean) // 3):
+            idx = i
+            print(l_speed_mean[idx]*3.6, "-", l_ACd[idx], " - ", l_f_aero[idx])
 
-    print("Valeur main en haut : Vitesse -  ACd - F")
-    for i in range(len(l_power_mean) // 2):
-        print(l_speed_mean[i]*3.6, "-", l_ACd[i], " - ", l_f_aero[i])
-
-    print("Valeur main en bas : Vitesse - ACd - F")
-    for i in range(len(l_power_mean) // 2):
-        idx = i+len(l_power_mean) // 2
-        print(l_speed_mean[idx]*3.6, "-", l_ACd[idx], " - ", l_f_aero[idx])
+        print("Valeur main en bas : Vitesse - ACd - F")
+        for i in range(2 * len(l_power_mean) // 3, len(l_power_mean)):
+            idx = i
+            print(l_speed_mean[idx]*3.6, "-", l_ACd[idx], " - ", l_f_aero[idx])
+        print("\n")
+        print("\n")
--- utls/extract_data.py
+++ utls/extract_data.py
@@ -48,7 +48,7 @@ def create_visu_vec(size, nb_sec_d, nb_sec_f):
     return vis_int
 
 
-def extract_with_pandas(f_name):
+def extract_cedric_data(f_name):
     # version specifique data Cedric
     df = pd.read_csv(f_name)
     l_25hv = df['25hv'].astype(float)
@@ -71,5 +71,74 @@ def extract_with_pandas(f_name):
 
     l_power = [l_25hp_m, l_35hp_m, l_25bp_m, l_35bp_m]
     l_speed = [l_25hv_m, l_35hv_m, l_25bv_m, l_35bv_m]
+
+    return l_power, l_speed
+
+
+def extract_with_pandas(f_name):
+    # version specifique data Cedric
+    df = pd.read_csv(f_name)
+
+    l_25hv = df['25hv'].astype(float)
+    l_25hp = df['25hp'].astype(float)
+    l_30hv = df['30hv'].astype(float)
+    l_30hp = df['30hp'].astype(float)
+    l_35hv = df['35hv'].astype(float)
+    l_35hp = df['35hp'].astype(float)
+    l_40hv = df['40hv'].astype(float)
+    l_40hp = df['40hp'].astype(float)
+
+    l_25pv = df['25pv'].astype(float)
+    l_25pp = df['25pp'].astype(float)
+    l_30pv = df['30pv'].astype(float)
+    l_30pp = df['30pp'].astype(float)
+    l_35pv = df['35pv'].astype(float)
+    l_35pp = df['35pp'].astype(float)
+    l_40pv = df['40pv'].astype(float)
+    l_40pp = df['40pp'].astype(float)
+
+    l_25bv = df['25bv'].astype(float)
+    l_25bp = df['25bp'].astype(float)
+    l_30bv = df['30bv'].astype(float)
+    l_30bp = df['30bp'].astype(float)
+    l_35bv = df['35bv'].astype(float)
+    l_35bp = df['35bp'].astype(float)
+    l_40bv = df['40bv'].astype(float)
+    l_40bp = df['40bp'].astype(float)
+
+    l_25hv_m = np.mean(l_25hv)
+    l_25hp_m = np.mean(l_25hp)
+    l_30hv_m = np.mean(l_30hv)
+    l_30hp_m = np.mean(l_30hp)
+    l_35hv_m = np.mean(l_35hv)
+    l_35hp_m = np.mean(l_35hp)
+    l_40hv_m = np.mean(l_40hv)
+    l_40hp_m = np.mean(l_40hp)
+
+    l_25pv_m = np.mean(l_25pv)
+    l_25pp_m = np.mean(l_25pp)
+    l_30pv_m = np.mean(l_30pv)
+    l_30pp_m = np.mean(l_30pp)
+    l_35pv_m = np.mean(l_35pv)
+    l_35pp_m = np.mean(l_35pp)
+    l_40pv_m = np.mean(l_40pv)
+    l_40pp_m = np.mean(l_40pp)
+
+    l_25bv_m = np.mean(l_25bv)
+    l_25bp_m = np.mean(l_25bp)
+    l_30bv_m = np.mean(l_30bv)
+    l_30bp_m = np.mean(l_30bp)
+    l_35bv_m = np.mean(l_35bv)
+    l_35bp_m = np.mean(l_35bp)
+    l_40bv_m = np.mean(l_40bv)
+    l_40bp_m = np.mean(l_40bp)
+
+    l_power = [l_25hp_m, l_30hp_m, l_35hp_m, l_40hp_m,
+               l_25pp_m, l_30pp_m, l_35pp_m, l_40pp_m,
+               l_25bp_m, l_30bp_m, l_35bp_m, l_40bp_m]
+
+    l_speed = [l_25hv_m, l_30hv_m, l_35hv_m, l_40hv_m,
+               l_25pv_m, l_30pv_m, l_35pv_m, l_40pv_m,
+               l_25bv_m, l_30bv_m, l_35bv_m, l_40bv_m]
 
     return l_power, l_speed
